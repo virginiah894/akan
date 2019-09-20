0d8d1f86ca213235abadd8966c01cb0764bae09e
virginiah894 <virgyperry@gmail.com>
2019-09-20 17:50:14 +0300

    addfunction for dob

--- akan/js/javascript.js
+++ akan/js/javascript.js
@@ -4,22 +4,27 @@ alert("isn't this cool?");
 prompt("Are you ready?");
 };
 
-function find(){
-  var s=document.querySelector("#sun");
-  var m=document.querySelector("#mon");
-  var t=document.querySelector("#tue");
-  var w=document.querySelector("#wed");
-  var t=document.querySelector("#thu");
-  var f=document.querySelector("#fri");
-  var sa=document.querySelector("#sat");
-  var gender=document.querySelector("#gen");
-  var print;
+function check(){
+  var find=document.getElementById("dob");
+  if(find.value<=0 || find.value>31){
+    alert('Invalid Date of Birth Entered');
+ return false;
+  }
+}
+//   var m=document.querySelector("#mon");
+//   var t=document.querySelector("#tue");
+//   var w=document.querySelector("#wed");
+//   var t=document.querySelector("#thu");
+//   var f=document.querySelector("#fri");
+//   var sa=document.querySelector("#sat");
+//   var gender=document.querySelector("#gen");
+//   var print;
  
-if (sun=="Male"){
-print=kwasi;
-}
-else if(sun=="Female"){
-  print=Akosua;
-}
-document.querySelector("#Result").innerHTML=print;
-}
+// if (sun=="Male"){
+// print=kwasi;
+// }
+// else if(sun=="Female"){
+//   print=Akosua;
+// }
+// document.querySelector("#Result").innerHTML=print;
+// }
