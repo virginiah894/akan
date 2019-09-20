db963441ab4d20be296c9f94cbdf747546e68622
virginiah894 <virgyperry@gmail.com>
2019-09-20 17:10:16 +0300

    add months of they year options

--- akan/js/javascript.js
+++ akan/js/javascript.js
@@ -4,5 +4,22 @@ alert("isn't this cool?");
 prompt("Are you ready?");
 };
 
- document.querySelector("#dyb")
+function find(){
+  var s=document.querySelector("#sun");
+  var m=document.querySelector("#mon");
+  var t=document.querySelector("#tue");
+  var w=document.querySelector("#wed");
+  var t=document.querySelector("#thu");
+  var f=document.querySelector("#fri");
+  var sa=document.querySelector("#sat");
+  var gender=document.querySelector("#gen");
+  var print;
  
+if (sun=="Male"){
+print=kwasi;
+}
+else if(sun=="Female"){
+  print=Akosua;
+}
+document.querySelector("#Result").innerHTML=print;
+}
