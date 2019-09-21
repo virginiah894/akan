14a31a5f01123943e22d62e43f2e18c659a12e25
virginiah894 <virgyperry@gmail.com>
2019-09-21 06:47:39 +0300

    parseInt values in the form

--- js/javascript.js
+++ js/javascript.js
@@ -5,7 +5,7 @@ prompt("Are you ready?");
 };
 
 function check(){
-  var find=document.getElementById("dob");
+  var find=parseInt(document.getElementById("dob"));
   if(find.value<=0 || find.value>31){
     alert('Invalid Date of Birth Entered');
  return false;
@@ -13,12 +13,25 @@ function check(){
 }
 
 function checkMonth(){
-  var con=document.getElementById("mob");
+  var con= parseInt(document.getElementById("mob"));
   if(con.value<=0 || con.value>12){
     alert('Invalid Date of Birth Entered');
  return false;
   }
 }
+// function to calculate the day of week
+function dayOfWeek(){
+  Day of the week (d) = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) mod 7
+
+var days= ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+if()
+
+
+
+
+
+
+};
 //   var m=document.querySelector("#mon");
 //   var t=document.querySelector("#tue");
 //   var w=document.querySelector("#wed");
